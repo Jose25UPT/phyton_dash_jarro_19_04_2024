68b71203e997a385b9cc38c3189c0f20195b9903
Jose25UPT <90207645+Jose25UPT@users.noreply.github.com>
2024-04-20 10:06:00 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,10 +1,19 @@
 import streamlit as st
-import numpy as np
+import pandas as pd
 import plotly.express as px
 
 st.title('Mi Primer Panel Streamlit')
 
-value = st.slider('Seleccione un valor', min_value=0, max_value=1000, value=500)
-data = np.random.randn(value)
+# Cargar los datos desde el archivo Excel
+file_path = 'ruta/a/tu/archivo.xlsx'  # Cambia 'ruta/a/tu/archivo.xlsx' por la ruta de tu archivo Excel
+df = pd.read_excel(file_path)
+
+# Filtrar los datos según la columna deseada (por ejemplo, 'Ejecucion al I Trimestre')
+selected_column = 'Ejecucion al I Trimestre'
+data = df[selected_column]
+
+# Crear el histograma utilizando Plotly Express
 fig = px.histogram(x=data)
+
+# Mostrar el histograma en la aplicación Streamlit
 st.plotly_chart(fig)
