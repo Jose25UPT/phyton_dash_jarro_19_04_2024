59e1cad4c1ed0d7a80416e10f6a8fa77db78f126
Jose25UPT <90207645+Jose25UPT@users.noreply.github.com>
2024-04-20 10:07:37 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,19 +1,26 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import os
 
 st.title('Mi Primer Panel Streamlit')
 
-# Cargar los datos desde el archivo Excel
-file_path = 'ruta/a/tu/archivo.xlsx'  # Cambia 'ruta/a/tu/archivo.xlsx' por la ruta de tu archivo Excel
+# Obtener la lista de archivos en la carpeta
+folder_path = 'excel_2014'  # Ruta de la carpeta que contiene los archivos Excel
+excel_files = [file for file in os.listdir(folder_path) if file.endswith('.xlsx')]
+
+# Permitir al usuario seleccionar un archivo Excel
+selected_file = st.selectbox('Selecciona un archivo Excel:', excel_files)
+
+# Cargar los datos desde el archivo seleccionado
+file_path = os.path.join(folder_path, selected_file)
 df = pd.read_excel(file_path)
 
-# Filtrar los datos según la columna deseada (por ejemplo, 'Ejecucion al I Trimestre')
-selected_column = 'Ejecucion al I Trimestre'
-data = df[selected_column]
+# Permitir al usuario seleccionar una columna para el histograma
+selected_column = st.selectbox('Selecciona una columna para el histograma:', df.columns)
 
 # Crear el histograma utilizando Plotly Express
-fig = px.histogram(x=data)
+fig = px.histogram(x=df[selected_column])
 
 # Mostrar el histograma en la aplicación Streamlit
 st.plotly_chart(fig)
